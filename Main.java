596e1b64020d0c8c88dddc5df4c99afe46523386
Roberio Moreira <86244226+roberiomoreira@users.noreply.github.com>
2023-04-25 16:01:08 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package Q65BB;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Pa o=null;
+		try {
+		
+			o=new Qb("a"," ","c");			
+		}
+		catch (Exception e) {
+			System.out.print("***Erro***");	
+		}
+		finally {
+			if(o!=null)			
+				System.out.print(o.get());
+		}
+	}
+
+}
